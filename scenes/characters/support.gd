3880ce9d0e7e8a52b72a2bd1bd9891f6a8456ef1
BenjaMad <benjamin.madrid@ug.uchile.cl>
2026-08-20 20:42:11 -0400

    agregar animaciones de movimiento a personaje soporte

--- scenes/characters/support.gd
+++ scenes/characters/support.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 3.5
 const JUMP_VELOCITY = 3.0
+@onready var animated_sprite_3d: AnimatedSprite3D = $AnimatedSprite3D
 
 
 func _physics_process(delta: float) -> void:
@@ -19,10 +20,16 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		animated_sprite_3d.play("Walk")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		animated_sprite_3d.play("Idle")
+		velocity.x = 0
+		velocity.z = 0
 
 	move_and_slide()
+	
+	if abs(velocity.x) > 0.1:
+		animated_sprite_3d.flip_h = velocity.x < 0
+	
