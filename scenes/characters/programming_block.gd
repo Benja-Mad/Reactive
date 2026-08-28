388846b8afd0ff1683050447f24b94887dfae23a
Benja-Mad <benjamin.madrid@ug.uchile.cl>
2026-08-28 13:08:32 -0400

    intento de ataques de fuego

---
+++ scenes/characters/programming_block.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+
+@onready var text_edit: TextEdit = $TextEdit
+@onready var timer: Timer = $Timer
+
+func _on_timer_timeout():
+	pass
+	
---
+++ scripts/fire_attack.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+@export var speed : float = 10.0
+@export var damage: float = 5.0
+
+var direction : Vector3
+
+func _physics_process(delta: float) -> void:
+	global_position = global_position + speed * direction * delta
