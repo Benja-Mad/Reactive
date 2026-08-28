ad973ffecc520af73683354a36bf1470deba8aaa
Benja-Mad <benjamin.madrid@ug.uchile.cl>
2026-08-28 00:21:14 -0400

    agregar sync timer

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -8,12 +8,18 @@ const JUMP_VELOCITY: float = 3.0
 @onready var label_3d: Label3D = $Label3D
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var input_synchronizer: InputSynchronizer = $InputSynchronizer
+@onready var sync_timer: Timer = $SyncTimer
 
+
+func _ready() -> void:
+	sync_timer.timeout.connect(_on_sync_timeout)
 
 func setup(player_data: Statics.PlayerData) -> void:
 	label_3d.text = player_data.name
 	set_multiplayer_authority(player_data.id)
 	camera_3d.current = is_multiplayer_authority()
+	if is_multiplayer_authority():
+		sync_timer.start()
 
 
 func _physics_process(delta: float) -> void:
@@ -43,4 +49,11 @@ func _physics_process(delta: float) -> void:
 	
 	if abs(velocity.x) > 0.1:
 		animated_sprite_3d.flip_h = velocity.x < 0
+	
+func _on_sync_timeout() -> void:
+	_sync(global_position, velocity)
+	
+func _sync(pos: Vector3, vel: Vector3) -> void:
+	global_position = global_position.lerp(pos, 0.5)
+	velocity = velocity.lerp(vel, 0.5)
 	
